5b7bcaedc3d4ca1a53f03187648436ad061cb6bd
newGustavo <gustavonascimento1709@gmail.com>
2024-02-27 15:54:13 -0500

    criando os primeiros componentes

---
+++ vue-receitas/src/components/SelecionaIngredientes.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import type ICategoria from "@/interfaces/ICategoria";
+  import { obterCategorias } from '@/http/index.ts';
+import CardCategoria from "./CardCategoria.vue";
+  export default {
+    data() {
+        return {
+            categorias: [] as ICategoria[]
+        };
+    },
+    async created() {
+        this.categorias = await obterCategorias();
+    },
+    components: { CardCategoria }
+}
+</script>
+
+<template>
+    <section class="selecionar-ingredientes">
+        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+
+        <p class="paragrafo-lg intrucoes">
+            Selecione abaixo os ingredientes que você quer usar nesta receita:
+        </p>
+        
+        <ul class="categorias">
+            <li v-for="categoria in categorias" :key="categoria.nome">
+              <CardCategoria :categoria="categoria" />
+            </li>
+        </ul>
+
+        <p class="paragrafo dica">
+            Consideramos que tenha sal, pimenta e água na sua casa!
+        </p>
+
+    </section>
+</template>
+
+<style scoped>
+.selecionar-ingredientes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.titulo-ingredientes {
+  color: var(--verde-medio, #3D6D4A);
+  display: block;
+  margin-bottom: 1.5rem;
+}
+
+.instrucoes {
+  margin-bottom: 2rem;
+}
+
+.categorias {
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+}
+
+.dica {
+  align-self: flex-start;
+  margin-bottom: 3.5rem;
+}
+
+@media only screen and (max-width: 767px) {
+  .dica {
+    margin-bottom: 2.5rem;
+  }
+}
+
+</style>
---
+++ vue-receitas/src/components/SuaLista.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+
+export default {
+    props: {
+        ingredientes: { type: Array as PropType<string[]>, required: true }
+    },
+    components: { Tag }
+}
+</script>
+
+<template>
+    <span class="subtitulo-lg su-lista-texto">
+        Sua Lista:
+    </span>
+
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+            <Tag :texto="ingrediente" ativa />
+        </li>
+    </ul>
+
+    <p v-else class="lista-vazia paragrafo">
+        <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de lupa.">
+        Sua lista não tem elementos!.
+    </p>
+</template>
+
+<style scoped>
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+</style>
